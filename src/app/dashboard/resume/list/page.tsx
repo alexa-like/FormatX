d8bbea9b2fdace574f7b78e64900dcc70748de07
Hayatulahi Yekeen Olamide <saudivpn8@gmail.com>
2026-08-19 09:04:53 +0100

    Add src/app/dashboard/resume/list/page.tsx

--- src/app/dashboard/resume/list/page.tsx
+++ src/app/dashboard/resume/list/page.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
+
+
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
